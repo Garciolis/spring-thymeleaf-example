6d9cbe792a42ce2166498d0f3bb089e9922b4d26
Angel <info@angelfrancisco.es>
2016-10-26 20:28:00 +0200

    Initial commit

---
+++ src/main/java/es/angelfrancisco/spring_thymeleaf_example/example/App.java
@@ -0,0 +1,13 @@
+package es.angelfrancisco.spring_thymeleaf_example.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
